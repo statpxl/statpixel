c910e6ce340ea98de908607254ce5f5bb88e9232
tildezero <suhasvoolla@gmail.com>
2021-07-24 04:14:04 +0000

    remove blue

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,4 @@
-<script>
-	export let name;
-</script>
-
 <main>
-	<h1 class="font-bold">Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1 class="font-bold">Statpixel</h1>
+	<p>A pretty decent website</p>
 </main>
-
